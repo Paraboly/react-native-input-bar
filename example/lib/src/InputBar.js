abc5eaa695485bfbba5b3f1a8a5f667599aaeded
FreakyCoder <kurayogun@gmail.com>
2019-12-18 15:39:49 +0300

    Library refactory is completed.

--- example/lib/src/InputBar.js
+++ example/lib/src/InputBar.js
@@ -2,35 +2,42 @@ import React from "react";
 import PropTypes from "prop-types";
 import { View, Dimensions, TextInput } from "react-native";
 import Androw from "react-native-androw";
-import styles from "./InputBar.style";
+import styles, {
+  _container,
+  _containerGlue,
+  _barIconContainer
+} from "./InputBar.style";
 import BarIcon from "./components/BarIcon";
 
 const { width: ScreenWidth } = Dimensions.get("window");
 
 const InputBar = props => {
   const {
+    width,
+    bottom,
+    height,
+    textColor,
+    shadowColor,
+    placeholder,
+    textInputStyle,
     primaryIconName,
     primaryIconType,
-    primaryIconColor,
     primaryIconSize,
+    backgroundColor,
+    primaryIconColor,
     secondaryIconName,
     secondaryIconType,
-    secondaryIconColor,
     secondaryIconSize,
     primaryIconOnPress,
+    secondaryIconColor,
     disableSecondaryIcon,
     secondaryIconOnPress
   } = props;
 
   renderBarIconContainer = () => (
-    <View
-      style={{
-        flexDirection: "row",
-        right: disableSecondaryIcon ? 8 : 16
-      }}
-    >
+    <View style={_barIconContainer(disableSecondaryIcon)}>
       {!disableSecondaryIcon && (
-        <View style={{ right: 12 }}>
+        <View style={styles.secondaryBarIconContainer}>
           <BarIcon
             name={secondaryIconName}
             type={secondaryIconType}
@@ -51,42 +58,11 @@ const InputBar = props => {
   );
 
   return (
-    <Androw
-      style={{
-        flex: 1,
-        justifyContent: "flex-end",
-        marginBottom: 24,
-        alignSelf: "center",
-        shadowRadius: 3,
-        shadowOpacity: 0.3,
-        shadowColor: "#757575",
-        shadowOffset: {
-          width: 0,
-          height: 1
-        }
-      }}
-    >
-      <View
-        style={{
-          height: 50,
-          paddingLeft: 16,
-          paddingRight: 32,
-          borderRadius: 12,
-          alignItems: "center",
-          flexDirection: "row",
-          width: ScreenWidth * 0.9,
-          backgroundColor: "#fdfdfd"
-        }}
-      >
+    <Androw style={_container(shadowColor, bottom)}>
+      <View style={_containerGlue(height, width, backgroundColor)}>
         <TextInput
-          placeholder="Type a message..."
-          style={{
-            fontSize: 16,
-            width: "95%",
-            marginRight: 8,
-            color: "#9da1ab",
-            fontWeight: "bold"
-          }}
+          placeholder={placeholder}
+          style={textInputStyle || _textInputStyle(textColor)}
         />
         {renderBarIconContainer()}
       </View>
@@ -95,25 +71,38 @@ const InputBar = props => {
 };
 
 InputBar.propTypes = {
+  textColor: PropTypes.string,
+  shadowColor: PropTypes.string,
+  placeholder: PropTypes.string,
+  primaryIconSize: PropTypes.number,
   primaryIconName: PropTypes.string,
   primaryIconType: PropTypes.string,
   primaryIconColor: PropTypes.string,
-  primaryIconSize: PropTypes.number,
+  secondaryIconSize: PropTypes.number,
   secondaryIconName: PropTypes.string,
   secondaryIconType: PropTypes.string,
   secondaryIconColor: PropTypes.string,
-  secondaryIconSize: PropTypes.number
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  bottom: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 };
 
 InputBar.defaultProps = {
-  primaryIconName: "send-o",
-  primaryIconType: "FontAwesome",
-  primaryIconColor: "#9da1ab",
+  bottom: 24,
+  height: 50,
   primaryIconSize: 21,
-  secondaryIconName: "attachment",
+  textColor: "#9da1ab",
+  secondaryIconSize: 21,
+  shadowColor: "#757575",
+  width: ScreenWidth * 0.9,
+  primaryIconName: "send-o",
+  backgroundColor: "#fdfdfd",
+  primaryIconColor: "#9da1ab",
   secondaryIconType: "Entypo",
   secondaryIconColor: "#9da1ab",
-  secondaryIconSize: 21
+  primaryIconType: "FontAwesome",
+  secondaryIconName: "attachment",
+  placeholder: "Type a message..."
 };
 
 export default InputBar;
